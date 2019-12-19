2a704838403c34b4cc51a9e357ff15b9d2825d2c
brownfieldbrown <55472237+brownfieldbrown@users.noreply.github.com>
2019-12-19 12:14:09 -0600

    Add files via upload

---
+++ js/loadpage.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+	setTimeout(() => { window.location.replace("hello_language.html"); }, 12000); // "3000" is the amount of time before the next page loads in milliseconds.
+
+});
